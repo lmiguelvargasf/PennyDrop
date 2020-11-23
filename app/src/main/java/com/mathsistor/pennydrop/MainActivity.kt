3f631668a1fc2f7eda01cd4dd4e176f7b2930e27
Mïk <m@mathsistor.com>
2020-11-23 12:38:37 -0500

    Set up nagivation with fragments

--- app/src/main/java/com/mathsistor/pennydrop/MainActivity.kt
+++ app/src/main/java/com/mathsistor/pennydrop/MainActivity.kt
@@ -2,10 +2,21 @@ package com.mathsistor.pennydrop
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navHostFragment = supportFragmentManager .findFragmentById(R.id.containerFragment) as NavHostFragment
+        navController = navHostFragment.navController
+        findViewById<BottomNavigationView>(R.id.bottom_nav).setupWithNavController(navController)
+
     }
 }
